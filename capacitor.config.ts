af961517a1c8c831d52eff7a385ebaf71970bb12
narsaabg@gmail.com <007@love>
2022-07-20 06:05:30 +0530

    push notification testing

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'i.love.ionic',
+  appName: 'ioreact',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
