624ef109d8b4dd84997dea3f0be138151fe16778
narsaabg <45451873+narsaabg@users.noreply.github.com>
2022-07-22 01:21:25 -0700

    Update Categories.tsx

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,9 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonRefresherContent,IonRefresher } from '@ionic/react';
+import { IonContent, IonHeader,IonImg ,IonCard,IonCardHeader,IonCardTitle,IonCardContent,IonCardSubtitle, IonGrid,IonPage,IonCol, IonRow,IonTitle, IonToolbar,IonRefresherContent,IonRefresher } from '@ionic/react';
 import {chevronDownCircleOutline,copy,share} from 'ionicons/icons';
 import { RefresherEventDetail } from '@ionic/core';
 import './Categories.css';
 
 const Categories: React.FC = () => {
+
+  const CategoriesList=[{
+    id:'1',
+    img:'https://picsum.photos/200',
+  },
+  {
+    id:'2',
+    img:'https://picsum.photos/200',
+  },
+  {
+    id:'3',
+    img:'https://picsum.photos/200',
+  },
+  {
+    id:'4',
+    img:'https://picsum.photos/200',
+  }];
 
   const doRefresh=(event: CustomEvent<RefresherEventDetail>)=>{
     setTimeout(() => {
@@ -29,6 +46,24 @@ const Categories: React.FC = () => {
               refreshingText="Refreshing...">
             </IonRefresherContent>
           </IonRefresher>
+          <IonGrid>
+          <IonRow>
+          {
+            CategoriesList.map((item,index)=>(
+              <IonCol size="4" key={index}>
+                <IonCard>
+                  <IonCardContent className="card-content-img">
+                    <IonImg src={item.img} alt="test"/>
+                     <IonCardHeader className="card-header">
+                    <IonCardSubtitle>Motivation</IonCardSubtitle>
+                  </IonCardHeader>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            ))
+          }
+          </IonRow>
+        </IonGrid>
         </IonContent>
       </IonContent>
     </IonPage>
